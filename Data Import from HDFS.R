40904e04054064f0d767c2ba32a4f1bd8e7203b5
Manikandan <nmani1191@gmail.com>
2018-03-03 01:15:55 +0530

    Data Import from HDFS.R

---
+++ Data Import from HDFS.R
@@ -0,0 +1,12 @@
+# R function to read files from HDFS in custom way. This is inspration from read.hdfs function from rHadoopclient package.
+
+custom.read.hdfs <- function (hdfs.path, hadoop.cmd = "hadoop") 
+{
+  tmp.file <- sprintf("tmp_hadoop_%s.csv", as.numeric(Sys.time()))
+  cmd <- sprintf("%s fs -cat %s | perl -pe 's/\t/,/g' > %s", 
+                 hadoop.cmd, hdfs.path, tmp.file)
+  system(command = cmd)
+  df <- read.csv(tmp.file, header = F, stringsAsFactors = T, na.strings = c(" ","","NULL","null","NA","na"))
+  file.remove(tmp.file)
+  df
+}
